72b9ba8735893819643e8d8f3bb9649b0f3afbfd
Jared Horne <thomhorne@gmail.com>
2019-02-22 14:53:10 -0600

    reinstall faker

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host: 'ec2-18-223-29-31.us-east-2.compute.amazonaws.com',
+      host: '127.0.0.1',
       database: 'espn',
       user: 'me',
       password: 'coolbeans',
