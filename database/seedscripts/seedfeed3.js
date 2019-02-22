61bff14ec74cf2937a4e297e13005f0449e5f6a7
Jared Horne <thomhorne@gmail.com>
2019-02-22 14:57:06 -0600

    removing other seed files

--- database/seedscripts/seedfeed3.js
+++
@@ -1,2 +0,0 @@
-USE espn;
-COPY espn.feeds from '/private/tmp/seed1.csv' DELIMITER ',' CSV HEADER;
--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
       password: 'coolbeans',
     },
     seeds: {
-      directory: './database/seedscripts',
+      directory: './database/seedscripts/',
     },
   },
 
